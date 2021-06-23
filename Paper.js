dd3c78334a0c00720fb8abb6b3f63f655838926a
NihithaY <83964777+NihithaY@users.noreply.github.com>
2021-06-23 09:55:53 -0400

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,32 @@
+class Paper{
+    constructor(x,y,r){
+    var options={
+        //put a comma after each line
+        'restutution' : 0.3,
+        'friction': 5,
+		'density': 1.2,
+        isStatic: true
+    }
+
+    this.x = x;
+    this.y = y;
+    this.width = 20;
+    this.height = 20;
+    this.r = 100;
+
+    this.image=loadImage("paper.png")
+
+    this.body = Bodies.circle(this.x, this.y, (this.r-20/2), options)
+    World.add(world, this.body);
+    }
+    display(){
+    push()
+    ImageMode(CENTER)
+    image(this.image, 0, 0, this.width ,this.height )
+    ellipseMode(RADIUS);
+    ellipse(this.x, this.y, (this.r-40/2), options);
+
+
+    pop()
+    }
+}
